4bab8c126bb5be6c6775584446905786f1ec60fd
rami.atrouni <ramiatrouni@gmail.com>
2021-04-23 11:47:41 +0300

    Initial project

---
+++ dj-events-frontend/pages/about.js
@@ -0,0 +1,11 @@
+function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find latest DJ and other musical events</p>
+            <p>Version 1.0.0</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ dj-events-frontend/pages/events/add.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const addPage = () => {
+    return (
+        <div>
+            <h1>Add Page</h1>
+        </div>
+    )
+}
+
+export default addPage

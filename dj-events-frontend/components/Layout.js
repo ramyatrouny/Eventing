df2d1a3e2eb430551904ab877c32eeb6fac93076
rami.atrouni <ramiatrouni@gmail.com>
2021-04-29 18:30:30 +0300

    create 404 page

--- dj-events-frontend/components/Layout.js
+++ dj-events-frontend/components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ title, keywords, description, children }) {
     return (
         <div>
             <Head>
-                <title>{title}t</title>
+                <title>{title}</title>
                 <meta name="description" content={description} />
                 <meta name="keywords" content={keywords} />
             </Head>
